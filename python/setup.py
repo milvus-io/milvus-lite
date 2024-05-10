c87e9fc32f18c576669e9c35c572e5f6b8358db1
junjie.jiangjjj <junjie.jiang@zilliz.com>
2024-05-10 07:07:47 +0000

    Version to 2.4.3
    Signed-off-by: junjie.jiangjjj <junjie.jiang@zilliz.com>

--- python/setup.py
+++ python/setup.py
@@ -120,7 +120,7 @@ def parse_requirements(file_name: str) -> List[str]:
         ]
 
 setup(name='milvus-lite',
-      version='2.4.2',
+      version='2.4.3',
       description='A lightweight version of Milvus wrapped with Python.',
       author='Milvus Team',
       author_email='milvus-team@zilliz.com',
