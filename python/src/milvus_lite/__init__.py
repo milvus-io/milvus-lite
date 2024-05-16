49ce8ebdae7bbf80b1202741f575fe5746a14462
junjiejiangjjj <junjie.jiang@zilliz.com>
2024-05-16 10:35:11 +0800

    Add version info (#125)
    Signed-off-by: junjie.jiang <junjie.jiang@zilliz.com>

--- python/src/milvus_lite/__init__.py
+++ python/src/milvus_lite/__init__.py
@@ -9,3 +9,14 @@
 # is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
 # or implied. See the License for the specific language governing permissions and limitations under
 # the License.
+
+
+from contextlib import suppress
+from pkg_resources import DistributionNotFound, get_distribution
+
+
+__version__ = "0.0.0.dev"
+
+
+with suppress(DistributionNotFound):
+    __version__ = get_distribution("milvus_lite").version
